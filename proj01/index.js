245dfcb34138c6a48d4942b2823234367e39aa36
Eduardo Rosa <edu.betim@aol.com>
2022-02-07 22:58:56 -0300

    iniciando o projeto

---
+++ proj01/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+//estou dizendo para o express usar EJS como View engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.get("/",(req, res) => {
+
+    res.render("index");
+});
+
+app.listen(8000,()=>{console.log("App rodando!");});
